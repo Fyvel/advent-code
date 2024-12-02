a9b50597e19aac9deb757f2cf3dfdd70998ae1fa
fyvel <fdelage8000@gmail.com>
2024-12-02 18:41:49 +1000

    feat: add script to compare runs between node, rust & go
    feat: add Bun to challenge Node

---
+++ 2024/compare.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+	echo "Usage: $0 <target_folder> [number_of_runs]"
+	exit 1
+fi
+
+TARGET_DIR="$1"
+RUNS=${2:-5} # Default to 5 runs if not specified
+
+if [ ! -d "$TARGET_DIR" ]; then
+	echo "Error: Directory '$TARGET_DIR' does not exist"
+	exit 1
+fi
+
+cd "$TARGET_DIR" || exit 1
+
+# Go build
+if [ -f "main.go" ]; then
+	echo "Building Go executables..."
+	go build -o advent_code_2024_go main.go
+fi
+
+# Rust build
+if [ -f "main.rs" ]; then
+	echo "Building Rust executable..."
+	mkdir -p "src"
+	cp "main.rs" "src/main.rs"
+
+	# Generate Cargo.toml
+	cat <<EOF >"Cargo.toml"
+[package]
+name = "advent_code_2024_rust"
+version = "0.1.0"
+edition = "2021"
+
+[dependencies]
+tokio = { version = "1.28", features = ["full"] }
+EOF
+
+	cargo build --release
+	rm -rf "src" "Cargo.toml"
+	rm -rf "src" "Cargo.lock"
+fi
+
+echo "Running benchmarks in: $TARGET_DIR with $RUNS warmup runs"
+
+echo "🔵 - Go"
+if [ ! -f "main.go" ]; then
+	echo "❌ - I Gon't"
+else
+	#  Warmup runs
+	for i in $(seq 1 $RUNS); do ./advent_code_2024_go >/dev/null 2>&1; done
+	./advent_code_2024_go
+	{ time ./advent_code_2024_go; } 2>&1 | grep real | awk '{printf "	🚀 - %s\n", $2}'
+fi
+
+echo "🟠 - Rust"
+if [ ! -f "main.rs" ]; then
+	echo "❌ - I Rusn't"
+else
+	#  Warmup runs
+	for i in $(seq 1 $RUNS); do ./target/release/advent_code_2024_rust >/dev/null 2>&1; done
+	./target/release/advent_code_2024_rust
+	{ time ./target/release/advent_code_2024_rust; } 2>&1 | grep real | awk '{printf "	🚀 - %s\n", $2}'
+fi
+
+echo "⚪️ - Bun"
+if [ ! -f "index.js" ]; then
+	echo "❌ - I Bun't"
+else
+	#  Warmup runs
+	for i in $(seq 1 $RUNS); do bun run index.js >/dev/null 2>&1; done
+	bun run index.js
+	{ time bun run index.js; } 2>&1 | grep real | awk '{printf "	🚀 - %s\n", $2}'
+fi
+
+echo "🟢 - Node"
+if [ ! -f "index.js" ]; then
+	echo "❌ - I Noden't"
+else
+	#  Warmup runs
+	for i in $(seq 1 $RUNS); do node index.js >/dev/null 2>&1; done
+	node index.js
+	{ time node index.js; } 2>&1 | grep real | awk '{printf "	🚀 - %s\n", $2}'
+fi
--- README.md
+++ README.md
@@ -1,2 +1,100 @@
 # advent-code
+
+[Advent of Code](https://adventofcode.com/)
+
 let's do this
+
+## Run the kikimeter
+
+```sh
+# sh compare.sh <day> <number of runs (optional)>
+sh compare.sh 1 10
+```
+
+## Template
+
+### Node
+
+```js
+const fs = require("fs").promises;
+const path = require("path");
+
+const readData = async () => {
+  const data = await fs.readFile(path.join(__dirname, "data.txt"), "utf-8");
+  return data.split("\n");
+};
+
+const formatData = async (rows) => {};
+
+const part1 = () => {};
+
+const part2 = () => {};
+
+readData()
+  .then(formatData)
+  .then(async (formattedData) => {
+    part1(formattedData);
+    part2(formattedData);
+  })
+  .catch((err) => console.error("Get rekt:", err));
+```
+
+### Go
+
+```go
+func readData() ([]string, error) {
+	data, err := os.ReadFile(filepath.Join(".", "data.txt"))
+	if err != nil {
+		return nil, err
+	}
+	return strings.Split(string(data), "\n"), nil
+}
+
+func formatData(rows []string) { }
+
+func part1(){}
+
+func part2(){}
+
+func main() {
+	reports, err := readData()
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	formattedData := formatData(reports)
+	part1(formattedData)
+	part2(formattedData)
+}
+```
+
+### Rust
+
+```rs
+use std::fs;
+use std::path::Path;
+use std::error::Error;
+use std::collections::HashMap;
+use tokio;
+
+async fn read_data() -> Result<Vec<String>, Box<dyn Error>> {
+    let contents = fs::read_to_string(Path::new("data.txt"))?;
+    Ok(contents.lines().map(String::from).collect())
+}
+
+async fn format_data(rows: Vec<String>) -> () {}
+
+fn part1() -> () {}
+
+fn part2() -> () {}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+	let data = read_data().await?;
+	let formatted = format_data(data).await;
+	part1(formatted.clone());
+	part2(formatted);
+	Ok(())
+}
+```
