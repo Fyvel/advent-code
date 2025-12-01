88b619dfc370617de7faac21659a162150a67048
fyvel <fdelage8000@gmail.com>
2025-12-02 01:05:13 +1000

    Day 1: part 2

--- 2025/day1/main.go
+++ 2025/day1/main.go
@@ -25,19 +25,15 @@ func main() {
 
 	// fmt.Println(data)
 	part1(data)
+	part2(data)
 }
 
 func part1(data []string) {
 	count := 0
-	w := Wheel{
-		pos: 50,
-		min: 0,
-		max: 100,
-	}
+	w := NewWheel()
 
 	for _, row := range data {
-		direction := row[0]
-		distance, _ := strconv.Atoi(row[1:])
+		direction, distance := parseData(row)
 
 		switch direction {
 		case 'L':
@@ -54,25 +50,64 @@ func part1(data []string) {
 	fmt.Println("Password: ", count)
 }
 
+func parseData(row string) (byte, int) {
+	direction := row[0]
+	distance, _ := strconv.Atoi(row[1:])
+	return direction, distance
+}
+
+func part2(data []string) {
+	w := NewWheel()
+
+	for _, row := range data {
+		direction, distance := parseData(row)
+
+		switch direction {
+		case 'L':
+			w.decrement(distance)
+		case 'R':
+			w.increment(distance)
+		}
+	}
+
+	fmt.Println("Password: ", w.carry)
+}
+
+func NewWheel() *Wheel {
+	return &Wheel{
+		pos: 50,
+		min: 0,
+		max: 100,
+	}
+}
+
 type Wheel struct {
-	pos int
-	min int
-	max int
+	pos   int
+	min   int
+	max   int
+	carry int
 }
 
 func (w *Wheel) decrement(dist int) {
-	if (w.pos - dist) < w.min {
-		w.pos = w.pos + w.max - dist
-		return
+	for range dist {
+		w.pos--
+		if w.pos < w.min {
+			w.pos = w.max - 1
+		}
+		if w.pos == 0 {
+			w.carry++
+		}
 	}
-	w.pos -= dist
 }
 
 func (w *Wheel) increment(dist int) {
-	if (w.pos + dist) >= w.max {
-		w.pos = w.pos - w.max + dist
-		return
+	for range dist {
+		w.pos++
+		if w.pos >= w.max {
+			w.pos = 0
+		}
+		if w.pos == 0 {
+			w.carry++
+		}
 	}
-	w.pos += dist
-
 }
