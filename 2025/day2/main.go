97a3386033fb9e1368df9269ee3ded5120581067
fyvel <fdelage8000@gmail.com>
2025-12-02 17:41:23 +1000

    Day 2: Gift Shop

---
+++ 2025/day2/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+)
+
+func readData() ([]string, error) {
+	data, err := os.ReadFile(filepath.Join(".", "data.txt"))
+	if err != nil {
+		return nil, err
+	}
+	return strings.Split(string(data), ","), nil
+}
+
+func formatData(rows []string) [][]int {
+	intervals := make([][]int, len(rows))
+
+	for i, row := range rows {
+		intervalStr := strings.Split(row, "-")
+		firstId, _ := strconv.Atoi(intervalStr[0])
+		lastId, _ := strconv.Atoi(intervalStr[1])
+		intervals[i] = getIdsInRange(firstId, lastId)
+	}
+
+	return intervals
+}
+
+func getIdsInRange(firstId int, lastId int) []int {
+	allIds := []int{}
+
+	for i := firstId; i <= lastId; i++ {
+		allIds = append(allIds, i)
+	}
+
+	return allIds
+}
+
+func part1(data [][]int) {
+	invalidIds := [][]int{}
+	sum := 0
+
+	for i, interval := range data {
+		invalidIds = append(invalidIds, []int{})
+		for _, id := range interval {
+			idStr := strconv.Itoa(id)
+			if len(idStr)%2 != 0 {
+				continue
+			}
+
+			middle := len(idStr) / 2
+			left := idStr[:middle]
+			right := idStr[middle:]
+
+			if left == right {
+				invalidIds[i] = append(invalidIds[i], id)
+				sum += id
+			}
+		}
+	}
+
+	fmt.Println("Invalid IDs:", invalidIds)
+	fmt.Println("Sum:", sum)
+}
+
+// func part2() {}
+
+func main() {
+	data, err := readData()
+	if err != nil {
+		fmt.Println("Get rekt:", err)
+		return
+	}
+
+	formattedData := formatData(data)
+	part1(formattedData)
+	// part2(formattedData)
+}
