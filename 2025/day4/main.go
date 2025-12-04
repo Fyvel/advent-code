61137a1f047e3d2e4b95d8eabc0500fb127fb77b
fyvel <fdelage8000@gmail.com>
2025-12-04 19:33:48 +1000

    Day 4: part 2 & visuals

--- 2025/day4/main.go
+++ 2025/day4/main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"time"
 )
 
 func readData() ([]string, error) {
@@ -63,7 +64,85 @@ func part1(grid [][]string) {
 
 }
 
-// func part2() {}
+func part2(grid [][]string) {
+	totalRolls := 0
+	lastCount := -1
+
+	for lastCount != totalRolls {
+		lastCount = totalRolls
+
+		renderGrid(grid)
+		for r := range grid {
+			for c := range grid[r] {
+
+				coords := [2]int{r, c}
+				cell := grid[r][c]
+
+				if cell != "@" {
+					continue
+				}
+
+				directions := [][]int{{0, -1}, {-1, -1}, {-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}}
+				rollsCount := 0
+
+				for _, direction := range directions {
+					row := coords[0] + direction[0]
+					col := coords[1] + direction[1]
+
+					if row < 0 || row >= len(grid) || col < 0 || col >= len(grid[0]) {
+						continue
+					}
+
+					if grid[row][col] == "@" {
+						rollsCount++
+					}
+				}
+				if rollsCount < 4 {
+					totalRolls++
+					grid[r][c] = "x"
+				}
+			}
+		}
+
+		renderGrid(grid)
+		// replace `x`` with `.`` for smoother visual
+		for r := range grid {
+			for c := range grid[r] {
+				if grid[r][c] == "x" {
+					grid[r][c] = "."
+				}
+			}
+		}
+	}
+	fmt.Println("Total rolls in reach:", totalRolls)
+
+}
+
+func renderGrid(grid [][]string) {
+	blue := "\033[34m"
+	yellow := "\033[33m"
+	hotpink := "\033[38;5;205m"
+	reset := "\033[0m"
+
+	for r := range grid {
+		for c := range grid[r] {
+			cell := grid[r][c]
+			switch cell {
+			case ".":
+				fmt.Print(blue + cell + reset)
+			case "x":
+				fmt.Print(yellow + cell + reset)
+			case "@":
+				fmt.Print(hotpink + cell + reset)
+			default:
+				fmt.Print(cell)
+			}
+		}
+		fmt.Println()
+	}
+	fmt.Println()
+	time.Sleep(200 * time.Millisecond)
+}
 
 func main() {
 	data, err := readData()
@@ -74,5 +153,5 @@ func main() {
 
 	formattedData := formatData(data)
 	part1(formattedData)
-	// part2(formattedData)
+	part2(formattedData)
 }
